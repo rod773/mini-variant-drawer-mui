46517ce476555cd26dd7d7acb703f8fbf35f4c51
rod773 <rodrigoarcadio1@gmail.com>
2024-10-22 20:54:51 +0200

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as React from "react";
 import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import MuiDrawer from "@mui/material/Drawer";
-import MuiAppBar, { AppBarProps } from "@mui/material/AppBar";
+import MuiAppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
 import CssBaseline from "@mui/material/CssBaseline";
